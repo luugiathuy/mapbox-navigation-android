4c323d1f5084391b1eb93975e15f85e0cdc9cbe6
abhishek1508 <abhishek.kejriwal@mapbox.com>
2019-09-30 12:43:38 -0700

    add code to reset the timer to 0 when navigation is stopped

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/MapboxNavigation.java
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/MapboxNavigation.java
@@ -53,9 +53,6 @@ import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import retrofit2.Callback;
-import timber.log.Timber;
-
 import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.BANNER_INSTRUCTION_MILESTONE_ID;
 import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.NON_NULL_APPLICATION_CONTEXT_REQUIRED;
 import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.VOICE_INSTRUCTION_MILESTONE_ID;
@@ -432,6 +429,7 @@ public class MapboxNavigation implements ServiceConnection {
       MapboxMetricsReporter.disable();
       navigationService.stopSelf();
       navigationEventDispatcher.onNavigationEvent(false);
+      AccountsManagerImpl.getInstance(applicationContext).endNavigation();
     }
   }
 
